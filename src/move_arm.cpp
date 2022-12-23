62647eef975182de2674040ab9106925b0b72e30
root <serenapaneri2605988@gmail.com>
2022-12-23 18:07:25 +0000

    move_arm final

--- src/move_arm.cpp
+++ src/move_arm.cpp
@@ -1,11 +1,8 @@
 #include "exprob_ass2/move_arm.h"
 #include <unistd.h>
 #include <moveit/move_group_interface/move_group_interface.h>
-#include <exprob_ass2/ErlOracle.h>
 #include <exprob_ass2/Complete.h>
 
-void hint_callback(const exprob_ass2::ErlOracle msg);
-int collected = 0;
 bool complete = false;
 
 double marker_z1 = 0.0;
@@ -34,34 +31,61 @@ namespace KCL_rosplan {
         group.setGoalPositionTolerance(0.0001);
         group.setGoalOrientationTolerance(0.001);
         
-        if(msg->parameters[1] == "wp1"){
-            ros::param::get("wp1", marker_z1)
+        if(msg->parameters[1].value == "wp1"){
+            ros::param::get("wp1", marker_z1);
+            if (marker_z1 == 0.75) {
+                group.setNamedTarget("low");
+	        group.move();
+	        sleep(3.0);
+            }
+            else if (marker_z1 == 1.25) {
+                group.setNamedTarget("high");
+	        group.move();
+	        sleep(3.0);
+            }
         }
         
-        else if(msg->parameters[1] == "wp2"){
-            ros::param::get("wp2", marker_z2)
+        else if(msg->parameters[1].value == "wp2"){
+            ros::param::get("wp2", marker_z2);
+            if (marker_z2 == 0.75) {
+                group.setNamedTarget("low");
+	        group.move();
+	        sleep(3.0);
+            }
+            else if (marker_z2 == 1.25) {
+                group.setNamedTarget("high");
+	        group.move();
+	        sleep(3.0);
+            }
         }
         
-        else if(msg->parameters[1] == "wp3"){
-            ros::param::get("wp3", marker_z3)
+        else if(msg->parameters[1].value == "wp3"){
+            ros::param::get("wp3", marker_z3);
+            if (marker_z3 == 0.75) {
+                group.setNamedTarget("low");
+	        group.move();
+	        sleep(3.0);
+            }
+            else if (marker_z3 == 1.25) {
+                group.setNamedTarget("high");
+	        group.move();
+	        sleep(3.0);
+            }
         }
         
-        else if(msg->parameters[1] == "wp4"){
-            ros::param::get("wp4", marker_z4)
+        else if(msg->parameters[1].value == "wp4"){
+            ros::param::get("wp4", marker_z4);
+            if (marker_z4 == 0.75) {
+                group.setNamedTarget("low");
+	        group.move();
+	        sleep(3.0);
+            }
+            else if (marker_z4 == 1.25) {
+                group.setNamedTarget("high");
+	        group.move();
+	        sleep(3.0);
+            }
         }
-        
-        group.setNamedTarget("high");
-	group.move();
-	// sleep(3.0);
-	
-	std::cout << collected << std::endl;
-	
-	//if the hint has not been collected try another pose
-	// if (collected == 0){
-	group.setNamedTarget("low");
-	group.move();
-	sleep(3.0);
-	//    }
 	    
 	std::cout << "A new hint has been collected!" << std::endl;
         
@@ -87,15 +111,10 @@ namespace KCL_rosplan {
         ros::NodeHandle nh("~");
         KCL_rosplan::MoveArmInterface move_arm(nh);
         move_arm.runActionInterface();
-        ros::Subscriber hint_sub = nh.subscribe("/oracle_hint", 1000, hint_callback);
         ros::ServiceServer complete_service = nh.advertiseService("complete_hypo", complete_hypo);
         ros::AsyncSpinner spinner(1);
         spinner.start();
         sleep(2.0);
         return 0;
     }
-    
-    void hint_callback(const exprob_ass2::ErlOracle msg) {
-        collected = 1;
-        std::cout << collected << std::endl; 
-    }
+
